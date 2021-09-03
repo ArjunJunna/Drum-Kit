773eb05fbeebbe5ab4926e181481730c65f84a3a
ArjunJunna <arjun9852@gmail.com>
2021-09-03 16:45:22 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener('keydown', (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  console.log(audio);
+  if (!audio) return;
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+});
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
